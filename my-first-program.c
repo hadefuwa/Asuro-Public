edd9297b14e2c9baaa1156a23082337fa71a81dc
Hamed Adefuwa <62181704+hadefuwa@users.noreply.github.com>
2020-08-12 08:00:18 +0100

    Create my-first-program.c

---
+++ my-first-program.c
@@ -0,0 +1,15 @@
+//my first program
+
+#include “asuro.h”
+
+int main(void) 
+{
+    Init(); //initialise - initialises the microcontroller ports
+
+    StatusLED(RED); // small white led nex to switch - turn RED
+    //Valid parameter values are OFF, GREEN, RED or YELLOW
+
+    while(1);    //eternal loop - a way of preventing the program from 'ending'
+
+    return 0; //end the program - no errors
+}
