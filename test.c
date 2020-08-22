faa094b38d6138c52d0e644e2042a2023eaa00cf
Hamed Adefuwa <62181704+hadefuwa@users.noreply.github.com>
2020-08-22 02:57:24 +0100

    .

--- test.c
+++ test.c
@@ -1,44 +1,35 @@
-// Introduction to Motors - MotorDir & MotorSpeed - Part 3
-// Drive the Asuro Forward then stop and then reverse
+// Introduction to Motors - Spin left and right on a loop
+// Spin to the right with the right back light on, get asuro to spin to left with the left back light on - on a loop continuously
 
-#include "asuro.h" //<<<<<<<
+#include "asuro.h" 
 
 int main(void)
 {
     int i = 0;
+
     Init();
-
-    StatusLED(GREEN);
-    MotorDir(FWD, FWD);   //Valid parameters are FWD, RWD, BREAK and FREE
-    MotorSpeed(140, 150); //Can use any value from 0 - 255 (min 60 to move))
-
-    for (i = 0; i < 300; i++) //3ms * 300 = 900ms
+    while (1)
     {
-        Sleep(216); //sleep the processor for 3ms
+        StatusLED(GREEN);
+        MotorDir(FWD, RWD);
+        MotorSpeed(150, 150);
+        BackLED(OFF, ON);
+
+        for (i = 0; i < 300; i++) //900ms
+        {
+            Sleep(216);
+        }
+
+        StatusLED(RED);
+        MotorDir(RWD, FWD);
+        MotorSpeed(150, 150);
+        BackLED(ON, OFF);
+
+        for (i = 0; i < 300; i++) //900ms
+        {
+            Sleep(216);
+        }
     }
-
-    MotorDir(BREAK, BREAK);
-    MotorSpeed(0, 0);
-    StatusLED(RED);
-
-    for (i = 0; i < 50; i++) //3ms * 50 = 150ms
-    {
-        Sleep(216); //sleep the processor for 3ms
-    }
-
-    BackLED(ON, ON);
-    MotorDir(RWD, RWD);
-    MotorSpeed(140, 150);
-
-    for (i = 0; i < 300; i++) //3ms * 300 = 900ms
-    {
-        Sleep(216); //sleep the processor for 3ms
-    }
-
-    MotorDir(BREAK, BREAK);
-    MotorSpeed(0, 0);
-    StatusLED(OFF);
-    BackLED(OFF, OFF);
 
     return 0;
 }
